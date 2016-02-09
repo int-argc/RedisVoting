b52af16f711272758b10a0166e52f02f48281695
int-argc <int-argc@users.noreply.github.com>
2016-02-09 18:27:58 +0800

    added tests, removed files

---
+++ src/test/SetTests.java
@@ -0,0 +1,53 @@
+// test class for redis voting app
+
+import static org.junit.Assert.assertEquals
+import org.junit.Before;
+import org.junit.Test;
+
+public class SetTests {
+    private SetOperations so;
+    private static final String testkey = "testset";
+    private static final String[] testEntries = ["A", "B", "C"];
+    private static final int[] testVotes = [3, 5, 1];
+
+    @Before
+    public void initSetOperations() {
+        so = new SetOperations(testkey);
+    }
+
+    @Test(timeout=1000)
+    public void testIncrementAndScore() {
+        clearSet();
+        addData();
+        String entry = "A"
+        int oldScore = so.getScore(entry);
+        so.incrementScore(entry);
+        assertEquals(oldScore + " 1 = " + (oldScore + 1), oldScore + 1, so.getScore(entry));
+        clearSet();
+    }
+
+    @Test(timeout=1000)
+    public void testIsDescending() {
+        clearSet();
+        addData();
+        Set<String> rr = so.sortDesc();
+        String[] expectedSet = ["B", "A", "C"];
+        for (int i = 0; i < rr.length; i++) {
+            assertEquals("position [" + i + "] should be " + expectedSet[i], expectedSet[i], rr.get(i));    // does set have get method?
+        }
+        clearSet();
+    }
+
+
+    // helper functions
+    private void addData() {
+        for (int i = 0; i < testEntries.length; i++) {
+            so.add(testVotes[i], testEntries[i]);
+        }
+    }
+
+    private void clearSet() {
+        so.deleteSet(testkey);
+    }
+
+}
